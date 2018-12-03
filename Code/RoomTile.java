1699d85c5aa6a160e69dd0d76693ae6e6e777210
Tariq <43186550+Zamanien@users.noreply.github.com>
2018-12-03 15:35:43 +0100

    Ændringer til Room Tiles - Constructor til Room tilføjet

--- Code/RoomTile.java
+++ Code/RoomTile.java
@@ -8,4 +8,9 @@ public class RoomTile {
   // Maybe these are mutually exclusive, so it would be better with a RoomObject class that they extend, and then a RoomTile can contain one RoomObject? RoomObject can also be Drawer or some such, that can contain Items?
   boolean window;
   boolean wall;
+  
+  public RoomTile 
+  {
+  }
 }
+
